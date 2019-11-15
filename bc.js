b0d3b18a52a6c45b4bc67533df6f6a5b2e31725d
wargames2004 <57296578+wargames2004@users.noreply.github.com>
2019-11-15 10:01:43 +0100

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "-";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`👑سيرفر عسكرية👑`,"http://twitch.tv/S-F")
+client.user.setStatus("633649691195736084")
+});
+
+
+
+client.login("NjQ0NTk3MjQ4MjY0NTAzMzE4.Xc5WWA.xQMLVthK0AnUWjd0JuzRFE5b9DQ");
